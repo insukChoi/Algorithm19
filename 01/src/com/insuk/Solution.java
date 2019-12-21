b3ab049881ca2b545f487c80634bf3c5296c1e79
insukChoi <52choiinsuk>
2019-12-22 01:19:33 +0900

    주석 수정

--- 01/src/com/insuk/Solution.java
+++ 01/src/com/insuk/Solution.java
@@ -12,10 +12,10 @@ public class Solution {
      * k는 1 이상 number의 자릿수 미만인 자연수입니다.
      *
      * 입출력 예
-     * number	    k	return
-     * 1924	        2	94
-     * 1231234	    3	3234
-     * 4177252841	4   775841
+     * number	k	return
+     * 1924	2	94
+     * 1231234	3	3234
+     * 4177252841	4	775841
      */
     public String solution(String number, int k) {
         StringBuilder answer = new StringBuilder();
