6b6d4b22be90716017268df88201d0002d263e33
최인석 <infuk@choeinseog-ui-iMac.local>
2019-12-22 00:52:39 +0900

    01 commit

---
+++ 01/src/com/insuk/Main.java
@@ -0,0 +1,11 @@
+package com.insuk;
+
+public class Main {
+    public static void main(String[] args) {
+        String number = "4177252841";
+        int k = 4;
+
+        Solution s = new Solution();
+        System.out.println(s.solution(number, k));
+    }
+}
